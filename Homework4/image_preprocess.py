516d7e31843bd859cc3f1bfe864ee87125ed3c40
Yu-Chen Hsiao <ychsiao@gpuserv0.cadlab.edu>
2022-01-13 04:45:31 +0800

    add image_preprocessing.py

---
+++ Homework4/image_preprocess.py
@@ -0,0 +1,25 @@
+import os
+from PIL import Image
+import argparse
+
+def resize(image_path):
+    dirs = os.listdir(image_path)
+    for file in dirs:
+        img = Image.open(os.path.join(image_path, file))
+        (w, h) = img.size
+        lower_edge = min(w, h)
+        if lower_edge < 96:
+            print("Remove image %s (%d, %d)" % (file, w, h))
+            os.remove(os.path.join(image_path, file))
+
+def main(args):
+    image_path =args.input_dir
+    resize(image_path)
+
+    return
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input-dir', type=str, default='data/training_hr_images', help="input images path")
+    args = parser.parse_args()
+    main(args)
